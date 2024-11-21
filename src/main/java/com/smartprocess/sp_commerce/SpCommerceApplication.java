e3d5a2ee42dbdb6d22c057fdcef0a057ceb03288
Rodrigo <lucianorodrigocarvalho@gmail.com>
2024-11-21 07:29:11 -0300

    Project created and entities User

---
+++ src/main/java/com/smartprocess/sp_commerce/SpCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.smartprocess.sp_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartprocess/sp_commerce/SpCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartprocess.sp_commerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
